c39184f6e978b769279a42cefb524e7c70d9999b
Vinicius Campos <silvcamposvinicius@gmail.com>
2017-11-24 22:37:12 -0200

    Atualiza arquivo da topologia para o ataque.

--- topologies/service_denial_topology.py
+++ topologies/service_denial_topology.py
@@ -12,14 +12,21 @@ class ServiceDenialTopology( Topo ):
         Topo.__init__( self )
 
         # Add hosts and switches
-        host1 = self.addHost( 'h1',ip="10.0.0.1")
-        host2 = self.addHost( 'h2', ip="10.0.0.2")
-        host3 = self.addHost( 'h3', ip="10.0.0.3")
+        host1 = self.addHost( 'h1')
+        host2 = self.addHost( 'h2')
+        host3 = self.addHost( 'h3')
+        host4 = self.addHost( 'h4')
+        host5 = self.addHost( 'h5')
+        host6 = self.addHost( 'h6')
         switch = self.addSwitch('s1')
 
         # Add links
         self.addLink(host1, switch)
         self.addLink(host2, switch)
         self.addLink(host3, switch)
+        self.addLink(host4, switch)
+        self.addLink(host5, switch)
+        self.addLink(host6, switch)
+
 
 topos = { 'service-denial-topology': ( lambda: ServiceDenialTopology() ) }
